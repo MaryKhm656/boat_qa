d4ada786ef61330e37cbd2ded1b92e82905f4f24
MaryKhm656 <nanoman66656@gmail.com>
2025-06-18 12:37:57 +0300

    added oar class

---
+++ models/oar.py
@@ -0,0 +1,19 @@
+from wearable import Wearable
+
+class Oar(Wearable):
+    
+    def __init__(self, size: float, material: str, wear: float = 0):
+        super().__init__(wear)
+        self.size = size
+        self.material = material
+        
+    def __str__(self):
+        return f"Весло. Материал: {self.material}, Размер: {self.size}, Износ: {self.wear}/10"
+    
+    def get_eff(self):
+        if self.wear >= 10:
+            return 0
+        elif self.wear <=0:
+            return 1
+        else:
+            return 1 - self.wear / 10
