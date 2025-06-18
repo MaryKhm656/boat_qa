891b0db8bf987bf33e67112626819273444dbd31
MaryKhm656 <nanoman66656@gmail.com>
2025-06-18 12:42:26 +0300

    Added anchor class

---
+++ models/anchor.py
@@ -0,0 +1,18 @@
+from wearable import Wearable
+
+class Anchor(Wearable):
+    
+    def __init__(self, weight: float, size: float, wear: float = 0):
+        super().__init__(wear)
+        self.weight = weight
+        self.size = size
+        self.is_dropped = False
+        
+    def __str__(self):
+        return f"Якорь. Вес: {self.weight}кг, Размер: {self.size}м, Износ: {self.wear}/10"
+    
+    def drop_anchor(self):
+        self.is_dropped = True
+        
+    def raise_anchor(self):
+        self.is_dropped = False
