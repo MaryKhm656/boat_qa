b6ee9ff0016aff3b839c29508005d4a37c5fbd1e
MaryKhm656 <nanoman66656@gmail.com>
2025-06-18 19:44:20 +0300

    Added tests for oar

--- tests/test_oar.py
+++ tests/test_oar.py
@@ -9,3 +9,12 @@ def test_wear_of_oars_after_rowing():
 def test_efficiency_declines_with_wear():
     oar_1 = Oar(10, 'дерево', 5)
     assert oar_1.get_eff() == 0.5
+
+def test_wear_variability_only_when_rowing_stops():
+    oar_1 = Oar(10, 'дерево')
+    assert oar_1.wear == 0
+    oar_1.start_rowing()
+    oar_1.start_rowing()
+    assert oar_1.wear == 0
+    oar_1.stop_rowing()
+    assert oar_1.wear == 1
